3c4b735a54437989b3bdde08242c35be801c3c9f
AnglusWang <AnglusWang@163.com>
2016-06-18 22:27:48 +0800

    新建一个五子棋项目

---
+++ app/src/main/java/com/angluswang/backgammon/view/BackgammonPanel.java
@@ -0,0 +1,24 @@
+package com.angluswang.backgammon.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jeson on 2016/6/18.
+ * 自定义View
+ */
+
+public class BackgammonPanel extends View {
+    public BackgammonPanel(Context context) {
+        super(context);
+    }
+
+    public BackgammonPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BackgammonPanel(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
