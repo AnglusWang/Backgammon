e3b8d04645c063541acdf72ecfc1ad22d806eecd
AnglusWang <AnglusWang@163.com>
2016-06-19 08:47:06 +0800

    初始化测绘

--- app/src/main/java/com/angluswang/backgammon/view/BackgammonPanel.java
+++ app/src/main/java/com/angluswang/backgammon/view/BackgammonPanel.java
@@ -10,15 +10,41 @@ import android.view.View;
  */
 
 public class BackgammonPanel extends View {
+
+    private int mPanelWidth;
+    private float mLineHeight;
+    private int MAX_LINE = 10;
+
     public BackgammonPanel(Context context) {
         super(context);
     }
 
     public BackgammonPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setBackgroundColor(0x44ff0000);
     }
 
     public BackgammonPanel(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        int width = Math.min(widthSize, heightSize);
+
+        if (widthMode == MeasureSpec.UNSPECIFIED) {
+            width = heightSize;
+        }else if (heightMode == MeasureSpec.UNSPECIFIED) {
+            width = widthSize;
+        }
+
+        setMeasuredDimension(width, width);
     }
 }
